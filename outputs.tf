6324909231e34a22211428a64802e893ac52c71a
Lionel Hercot <lhercot@cisco.com>
2020-10-05 21:31:42 -0700

    Add Azure site

--- outputs.tf
+++ outputs.tf
@@ -8,11 +8,6 @@ output "vmware_portgroup" {
   description = "The name of the VMware portgroup used"
 }
 
-output "onprem_site_id" {
-  value = module.demo_onprem.onprem_site_id
-  description = "The id of the On-Prem Site"
-}
-
 output "db_gateway" {
   value = module.demo_template.db_gateway
   description = "The gateway IP address for On-premises network"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,21 @@
 variable "mso_username" {}
 variable "mso_password" {}
 variable "mso_url" {}
+
+variable "name_prefix" {
+  type = string
+  default = "TF-"
+  description = "A prefix that will be added to all the object names"
+}
+
+variable "schema_name" {
+  type = string
+  default = "terraform_hybrid_cloud"
+  description = "The name of the MSO schema to be created"
+}
+
+variable "tenant" {
+  type = string
+  default = "WoS"
+  description = "The MSO tenant to use for this configuration"
+}
