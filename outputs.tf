9018a27ada60d682d41f2dbe2c50d6384bd99da2
Lionel Hercot <lhercot@cisco.com>
2020-10-05 22:21:29 -0700

    Add necessary additional outputs

--- outputs.tf
+++ outputs.tf
@@ -13,7 +13,17 @@ output "db_gateway" {
   description = "The gateway IP address for On-premises network"
 }
 
-output "azure_subnet_dn" {
-  value = module.demo_azure.subnet_dn
-  description = "The ACI Azure subnet object DN"
+output "azure_subnet_name" {
+  value = module.demo_azure.subnet_name
+  description = "The ACI Azure subnet object name"
+}
+
+output "resource_group_name" {
+  value = module.demo_azure.resource_group_name
+  description = "The Azure resource_group_name"
+}
+
+output "virtual_network_name" {
+  value = module.demo_azure.virtual_network_name
+  description = "The Azure virtual network name"
 }
