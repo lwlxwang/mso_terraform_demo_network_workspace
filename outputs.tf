079bfed8ac5a52149954c9bed57a49049819d06e
Lionel Hercot <lhercot@cisco.com>
2020-10-05 21:49:29 -0700

    Add Azure subnet to outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "db_gateway" {
   value = module.demo_template.db_gateway
   description = "The gateway IP address for On-premises network"
 }
+
+output "azure_subnet_dn" {
+  value = module.demo_azure.subnet_dn
+  description = "The ACI Azure subnet object DN"
+}
