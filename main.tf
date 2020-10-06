5be808c56e547c0ffcb99dac6ba042e7cb9a195f
Lionel Hercot <lhercot@cisco.com>
2020-10-05 20:02:39 -0700

    Add missing variables

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ module "demo_template" {
   source  = "app.terraform.io/cisco-dcn-ecosystem/demo_template/mso"
   version = "0.0.1"
 
+  mso_username = var.mso_username
+  mso_password = var.mso_password
+  mso_url = var.mso_url
   name_prefix = "TF-"
   schema_name = "terraform_hybrid_cloud"
   subnet_gw = "10.101.10.254/24"
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "mso_username" {}
+variable "mso_password" {}
+variable "mso_url" {}
